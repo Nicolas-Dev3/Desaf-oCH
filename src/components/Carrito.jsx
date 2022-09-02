04d56c5987b8b407ee0cbd4781f366c6ef6a55db
Nicolas Quinteros <desarrollador.nicolas@gmail.com>
2022-09-01 22:48:16 -0300

    segundo commit

---
+++ src/components/Carrito.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Carrito = (props) => {
+    return (
+        <div> Nombre = {props.nombre} </div>
+    )
+}
+
+export default Carrito
---
+++ src/components/Carro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import icono from "../assets/carrito.png" 
+
+const Carro = () => {
+  return (
+    <div>
+        <img src={icono} alt="" />
+    </div>
+  )
+}
+
+export default Carro
